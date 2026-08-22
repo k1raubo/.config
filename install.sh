bf8a72883c8957bc7c0560710f42138635144759
Kamil Raubo <k1raubo@neuravers.com>
2026-08-22 14:01:34 +0200

    feat: Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+mkdir -p ~/.config ~/Pictures ~/.oh-my-zsh ~/environments
+
 cp -r "$SCRIPT_DIR/.config/." ~/.config/
 cp "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
+cp "$SCRIPT_DIR/.zshrc" ~/.zshrc
+cp -r "$SCRIPT_DIR/Pictures/." ~/Pictures/
+cp -r "$SCRIPT_DIR/.oh-my-zsh/." ~/.oh-my-zsh/
+cp -r "$SCRIPT_DIR/environments/." ~/environments/
 
 echo "Installed!"
